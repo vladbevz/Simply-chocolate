ffd1b12f29c4b4fdf7a8272504a60de54f689cd3
vbevz <122693446+vladbevz@users.noreply.github.com>
2025-02-23 13:48:14 +0100

    updates before deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
+  base: '/Simply-chocolate/',
   root: 'src',
   build: {
     rollupOptions: {
